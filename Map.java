caedf145de1150c186ac2d7dcfefede9a277a528
LisaJenny <67246209+LisaJenny@users.noreply.github.com>
2020-10-02 12:09:58 +0800

    Map.java更新

---
+++ Map.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class Map {
+
+	public static void main(String[] args) {
+		// Loading	
+		Scanner in =new Scanner(System.in);
+		//Geography Drawing
+		System.out.println("high=350,Long=750,Lautu=800");
+		System.out.println(in.nextLine());
+		System.out.println("moutain"+in.nextLine());
+		System.out.println("forest"+in.nextLine());
+		System.out.println("plain"+in.nextLine());
+		System.out.println("tree"+in.nextLine());
+		System.out.println("lake"+in.nectLine());
+		System.out.println("river"+in.nextLine());
+		System.out.println("biome"+in.nextLine());
+		System.out.println("sky high"+in.nextLine());
+		//Solve
+		System.out.println(in.nextLine());
+		System.out.println(in.nextLine());
+		System.out.println(in.nextLine());
+		System.out.println(in.nextLine());
+		System.out.println(in.nextLine());
+		System.out.println(in.nextLine());
+		System.out.println(in.nextLine());
+		//Map Loading
+		System.out.println("seed"+in.nextLine());
+		System.out.println("Geography loading");
+	}
+
+}
