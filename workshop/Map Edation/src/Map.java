2ea12ae23d1743f8a59d9ea193929b263b0523da
LisaJenny <67246209+LisaJenny@users.noreply.github.com>
2020-10-03 10:20:40 +0800

    Add files via upload

--- workshop/Map Edation/src/Map.java
+++ workshop/Map Edation/src/Map.java
@@ -28,7 +28,6 @@ public class Map {
 		//Map Loading
 		System.out.println("seed"+in.nextLine());
 		System.out.println("Geography loading");
-                System.out.println("Mem loading")
 	}
 
 }
